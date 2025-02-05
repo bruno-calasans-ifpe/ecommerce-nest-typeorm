5d7e4fb29e648ac33f65d79eaed3a073421e2a61
bruno-calasans-ifpe <brcl1@discente.ifpe.edu.br>
2025-02-05 14:45:19 -0300

    Fix: create custom errors

--- src/client/cliente.controller.ts
+++ src/client/cliente.controller.ts
@@ -4,13 +4,15 @@ import {
   Body,
   Get,
   Param,
-  HttpException,
-  HttpStatus,
   Put,
   Delete,
 } from '@nestjs/common';
 import { ClientService } from './cliente.service';
 import { ClientCreateData, ClientUpdateData } from './cliente.dto';
+import { InternalServerError } from 'src/errors/InternalServerErrorError';
+import { NotFoundError } from 'src/errors/NotFoundError';
+import { ConflictError } from 'src/errors/ConflictError';
+import { NotModifiedError } from 'src/errors/NotModifiedError';
 
 @Controller('client')
 export class ClientController {
@@ -21,31 +23,24 @@ export class ClientController {
     try {
       return this.clientService.getAll();
     } catch (error) {
-      throw new HttpException(
-        'Erro pegar todos os clientes',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+      throw new InternalServerError('Erro pegar todos os clientes');
     }
   }
 
   @Get(':id')
   async getClient(@Param() params: any) {
     const id = params.id;
-    if (!id)
-      throw new HttpException('Nenhum id fornecido', HttpStatus.BAD_REQUEST);
 
     const foundClient = await this.clientService.get(+id);
-    if (!foundClient)
-      throw new HttpException('Cliente não encontrado', HttpStatus.NOT_FOUND);
+    if (!foundClient) throw new NotFoundError('Cliente não encontrado');
 
-    return foundClient;
+    return { message: 'Cliente encontrado', client: foundClient };
   }
 
   @Post()
   async createClient(@Body() data: ClientCreateData) {
     const emailAlreadyInUse = await this.clientService.getByEmail(data.email);
-    if (emailAlreadyInUse)
-      throw new HttpException('Email já em uso', HttpStatus.CONFLICT);
+    if (emailAlreadyInUse) throw new ConflictError('Email já em uso');
 
     const createdClient = await this.clientService.create(data);
     return {
@@ -58,19 +53,12 @@ export class ClientController {
   async updateClient(@Param() params: any, @Body() data: ClientUpdateData) {
     const id = params.id;
 
-    if (!id)
-      throw new HttpException('Nenhum id fornecido', HttpStatus.BAD_REQUEST);
-
     const foundClient = await this.clientService.get(+id);
-    if (!foundClient)
-      throw new HttpException('Cliente não encontrado', HttpStatus.NOT_FOUND);
+    if (!foundClient) throw new NotFoundError('Cliente não encontrado');
 
     const result = await this.clientService.update(+id, data);
     if (result.affected === 0) {
-      throw new HttpException(
-        'Cliente não atualizado',
-        HttpStatus.NOT_MODIFIED,
-      );
+      throw new NotModifiedError('Cliente não atualizado');
     }
 
     return {
@@ -82,16 +70,13 @@ export class ClientController {
   @Delete(':id')
   async deleteClient(@Param() params: any) {
     const id = params.id;
-    if (!id)
-      throw new HttpException('Nenhum id fornecido', HttpStatus.BAD_REQUEST);
 
     const foundClient = await this.clientService.get(+id);
-    if (!foundClient)
-      throw new HttpException('Cliente não encontrado', HttpStatus.NOT_FOUND);
+    if (!foundClient) throw new NotFoundError('Cliente não encontrado');
 
     const result = await this.clientService.delete(+id);
     if (result.affected === 0) {
-      throw new HttpException('Cliente não removido', HttpStatus.NOT_MODIFIED);
+      throw new NotModifiedError('Cliente não atualizado');
     }
 
     return { message: 'Cliente removido com sucesso', client: foundClient };
